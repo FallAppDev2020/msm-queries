6aa89e8adff726bc41f55e50f18122b5f7eb113e
OLauterbach <olauterb@chicagobooth.edu>
2020-10-29 06:27:43 +0000

    Added basic tables Missing Director_id reference for actors and secondary pages

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,19 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all
+    render({ :template => "director_templates/index.html.erb"})
+  end
+
+  def oldest
+    @oldest = Director.where.not({ :dob => nil}).order({ :dob => :asc}).at(0)
+    @list_of_directors = Director.all
+    render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest = Director.where.not({ :dob => nil}).order({ :dob => :desc}).at(0)
+    @list_of_directors = Director.all
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+end
